c4f0088f81f0d53d82c71398ba81122b440fda2b
Ryan Bravo <rrbravo2@gmail.com>
2019-04-20 20:18:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,4 +15,6 @@ function deleteFromObjectByKey (object, key) {
   return obj
 }
 
-function destructivelyDeleteFromObjectByKey ()
+function destructivelyDeleteFromObjectByKey (object, key) {
+  
+}
